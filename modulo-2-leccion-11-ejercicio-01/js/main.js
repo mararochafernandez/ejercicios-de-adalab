0be968fc095ab3159fafceaae5a288da0a2f3ba3
Mara Rocha Fernández <Email@ejemplo.com>
2021-12-19 16:52:07 +0100

    add 2.11 exercises

--- modulo-2-leccion-11-ejercicio-01/js/main.js
+++ modulo-2-leccion-11-ejercicio-01/js/main.js
@@ -2,32 +2,35 @@
 
 /* EJERCICIO 1
 Vamos a explorar un API abierto de información sobre el mundo Star Wars. En esta página tenemos la documentación completa del API y un formulario que nos permite hacer peticiones a la URL que indiquemos. Identifica la siguiente información sobre SWAPI:
-- la URL base del API
-- si necesita autenticación
-- método HTTP que deben usar las peticiones a este API
-- URL para acceder a la info del personaje "Luke Skywalker"
-- URL para acceder a la info de la película "A New Hope"
-- a qué otros recursos puedo acceder desde el API además de personajes y pelis
-- URL para acceder al listado de personajes, ¿está paginada?
-- cómo puedo buscar personajes mediante la URL
-- cómo puedo hacer que el JSON de una petición se me devuelva traducido a Wookiee */
+1. la URL base del API
+2. si necesita autenticación
+3. método HTTP que deben usar las peticiones a este API
+4. URL para acceder a la info del personaje "Luke Skywalker"
+5. URL para acceder a la info de la película "A New Hope"
+6. a qué otros recursos puedo acceder desde el API además de personajes y pelis
+7. URL para acceder al listado de personajes, ¿está paginada?
+8. cómo puedo buscar personajes mediante la URL
+9. cómo puedo hacer que el JSON de una petición se me devuelva traducido a Wookiee */
 
-// URL base: 
+// 1. URL base: 
 const url = 'https://swapi.py4e.com/api/';
 
-// No necesita autenticación
+// 2. No necesita autenticación
 
-// Protocolo: HTTPS (Hypertext Transfer Protocol Secure)
+// 3. Método HTTP: GET
 
-// Método HTTP: GET
-
-// Info del personaje Luke Skywalker: 
+// 4. Info del personaje Luke Skywalker: 
 const urlLukeSkywalker = 'https://swapi.py4e.com/api/people/1/';
 
-// Info de la película A New Hope:
+// 5. Info de la película A New Hope:
 const urlANewHope = 'https://swapi.py4e.com/api/films/1/';
 
-// Buscar personajes por nombre (en minúsculas):
+// 6. Otros recursos: planets, species, starships, vehicles
+
+// 7. Listado de personajes (sí, está paginado):
+const urlPeople = 'https://swapi.py4e.com/api/people/?page=1';
+
+// 8. Buscar personajes por nombre o parte del nombre (en minúsculas):
 const urlSearchByName = 'https://swapi.py4e.com/api/people/?search=luke';
 
 // Traducir a Wookiee: ?format=wookiee
